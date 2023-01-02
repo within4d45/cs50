edd152fd5209057348b140f2ae0885ea44f18660
EfteYo <58327339+EfteYo@users.noreply.github.com>
2023-01-02 09:45:54 +0100

    first upload in new branch

--- projects/3/mail/mail/static/mail/inbox.js
+++ projects/3/mail/mail/static/mail/inbox.js
@@ -3,6 +3,11 @@ To do:
 ReCreate different views for inbox and outbox (doing that in the add email function by getting different parameters additionally)
 [] Loading E-Mails after sending will miss the just sent e-mail
 [] init emails with unread
+
+Questions:
+email-cards:
+- making the anchor element pointy - is that good code 
+- when we are in the overview, we have already fetched the e-mail contents - why do we want to fetch them again?
 */
 
 
@@ -20,14 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
   load_mailbox('inbox');
 });
 
-function read_email(contents) {
-    // Show the email and hide other views
-    document.querySelector('#emails-view').style.display = 'none';
-    document.querySelector('#read-email').style.display = 'block';
-    document.querySelector('#compose-view').style.display = 'none';
 
-    document.querySelector('#read-email').innerHTML = `<div>From:</div>`;
-}
 
 function compose_email() {
   // Show compose view and hide other views
@@ -67,34 +65,74 @@ function send_email() {
       body: document.querySelector('#compose-body').value
     })
   })
-    .then(response => response.json())
-    .then(result => {
-      // Print result
-      console.log(result);
-    });
 
   return false;
 }
 
+function load_email(email_id) {
+  // Show the email and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#read-email').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  document.querySelector('#read-email').innerHTML = ``;
+
+  fetch('/emails/' + email_id)
+  .then(response => response.json())
+  .then(email => {
+    read_email(email);
+  })
+
+  fetch('/emails/' + email_id, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: false
+    })
+  })
+}
+
 function add_email(contents) {
-  email = document.createElement('div');
-  email.className = 'email-view';
+  const email = document.createElement('a');
+  email.className = 'email-preview';
 
-  email.innerHTML = `<div class="info">
-                      <div class="participants">
-                        <div class="from"><b>From:</b> ${contents.sender}</div>
-                        <div class="to"><b>To:</b> ${contents.recipients}</div>
-                      </div>
-                      <div class="timestamp"><b>Date:</b> ${contents.timestamp}</div>
-                    </div>
-                    <div class="subject">${contents.subject}</div>
-                    <div class="body">${contents.body.slice(0, 30)}</div>
-                    <button onclick="read_email(${contents.id})">Read Email<button>`;
+  email.innerHTML = `
+  <div class="info">
+    <div class="participants">
+      <div class="from"><b>From:</b> ${contents.sender}</div>
+      <div class="to"><b>To:</b> ${contents.recipients}</div>
+    </div>
+    <div class="timestamp"><b>Date:</b> ${contents.timestamp}</div>
+  </div>
+  <div class="subject">${contents.subject}</div>
+  <div class="body">${contents.body.slice(0, 30)}</div>`;
 
-  if (contents.read === true) {
+
+  if (contents.read === false) {
     email.style.backgroundColor = "lightgrey";
   }
 
+  /* Pass the id to be fetched in the next function */
+  email.addEventListener('click', function() {
+    load_email(contents.id);
+  });
+
   // Add email to DOM
   document.querySelector('#emails-view').append(email);
+}
+
+function read_email(contents) {
+  const email = document.createElement('div');
+  email.className = 'email-content';
+  email.innerHTML = `
+  <div class="from"><b>From:</b> ${contents.sender}</div>
+  <div class="to"><b>To:</b> ${contents.recipients}</div>
+  <div class="subject"><b> Subject:</b>${contents.subject}</div>
+  <div class="timestamp"><b>Timestamp:</b> ${contents.timestamp}</div>
+  <button class="btn btn-sm btn-outline-primary" id="reply">Reply</button>
+  <hr>
+  <div class="body">${contents.body}</div>`;
+
+  console.log(contents);
+
+  document.querySelector('#read-email').append(email);
 }
